5edf498a3f32ca932d6ea9b3b156f8dca5eabe9d
E. Almqvist <elalmqvist@gmail.com>
2022-07-03 00:52:40 +0200

    Fixed ActiveLink coloring

--- components/activelink.tsx
+++ components/activelink.tsx
@@ -7,6 +7,7 @@ import styled, {css} from "styled-components"
 const ALink = styled.a<{active?: boolean}>`
 	border-bottom: 1px solid transparent;
 	${({active}) => active && css`
+		color: var(--fg) !important;
 		border-bottom-color: currentColor;
 	`}
 `
