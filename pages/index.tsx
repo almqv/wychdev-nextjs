6ef4f29b84aba7dea6cc47ba387c69e0bf834546
E. Almqvist <elalmqvist@gmail.com>
2022-07-02 19:28:46 +0200

    Removed more junk

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,6 @@
-// Stuff
 import type {NextPageWithLayout} from './_app'
 import type { ReactElement } from 'react'
 import Layout from '../components/layout'
-
-// Components
-// import Head from 'next/head'
-// import styles from '../styles/Home.module.css'
-// import Header from 'components/header'
-
 
 const Page: NextPageWithLayout = () => {
 	return (
@@ -19,20 +12,4 @@ Page.getLayout = (page: ReactElement) => {
 	return <Layout>{page}</Layout>
 }
 export default Page
-
-// const Home: NextPage = () => {
-// 	return (
-// 		<div className={styles.container}>
-// 			<Head>
-// 				<title>wych.dev</title>
-// 				<link rel="icon" href="/favicon.ico" />
-// 			</Head>
-// 
-// 			<Header />
-// 		</div>
-// 	)
-// }
-
-//export default Home
-
 
