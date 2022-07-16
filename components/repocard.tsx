c660cd8758a0fb9f56c2cac19a24cab124a7dd1b
E. Almqvist <elalmqvist@gmail.com>
2022-07-16 18:13:36 +0200

    Repocard etc

---
+++ components/repocard.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+// import Link from "next/link"
+import React, {useState} from "react"
+
+const RepoCardCont = styled.div`
+	display: flex;
+`
+
+const RepoCard = () => {
+	return (
+		<RepoCardCont>
+
+		</RepoCardCont>
+	)
+}
+
+export default RepoCard
