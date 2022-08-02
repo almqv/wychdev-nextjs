37d296394f566933f083c29be054da3aabe58cd3
E. Almqvist <elalmqvist@gmail.com>
2022-08-02 21:34:58 +0200

    Removed stuff

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -2,15 +2,13 @@ import type {NextPageWithLayout} from './_app'
 import type { ReactElement } from 'react'
 import Layout from '../components/layout'
 import { Section } from './index'
-import Link from "next/link"
+// import Link from "next/link"
 
 const Page: NextPageWithLayout = () => {
 	return (
 		<>
 			<Section>
-				<h2>Page under development...</h2>
-				<p>While you wait, why not check out my <a href="https://github.com/E-Almqvist">GitHub</a>? Or perhaps my very own <a href="https://git.wych.dev">git-server</a>?</p>
-				<p>Or you could also <Link href="/">go back</Link> to the home page.</p>
+				<h2>GitHub Repositories</h2>
 			</Section>
 		</>
 	)
